567da7afed8966481c8f6b88cf705c0435eb3196
Efornies <eleforbel@gmail.com>
2022-09-08 12:39:15 +0000

    visual

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,21 +2,28 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
 
-	return (
-		
-		<div className="parent h-100"> 
-		<div className="back "> </div>
-			<div className="art"> 
-				<div className="welcomebox">
-					<div className="text-center mt-5">
-						<h1>Artcop</h1>
-						<p> Entra en el mundo colaborativo de ARTCOP</p>
-					</div>
-				</div>
-			</div>
-		</div> 
-	);
+
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="parent h-100">
+      <div className="back "> </div>
+      <div className="art">
+        <div className="welcomebox">
+          <div className="loginform">
+            <div className="titulo text-center mt-5">
+				<div>
+					Artcop 
+				 </div>
+              <div>
+                <p> Entra en el mundo colaborativo de ARCOP</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
