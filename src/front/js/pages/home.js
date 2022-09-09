8fdcb1f0d45743d3e94ef2db6081a134702334a1
Efornies <eleforbel@gmail.com>
2022-09-09 12:26:03 +0000

    por fin cuadro de login

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,45 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-
-
 export const Home = () => {
   const { store, actions } = useContext(Context);
-
   return (
     <div className="parent h-100">
       <div className="back "> </div>
       <div className="art">
         <div className="welcomebox">
           <div className="loginform">
-            <div className="titulo text-center mt-5">
-				<div>
-					Artcop 
-				 </div>
-              <div>
-                <p> Entra en el mundo colaborativo de ARCOP</p>
-              </div>
-            </div>
+          <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
           </div>
         </div>
       </div>
