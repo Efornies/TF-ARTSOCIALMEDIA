d531e708def97b520217b0cfe5b4d9664441abc3
Efornies <eleforbel@gmail.com>
2022-10-04 17:17:25 +0000

    cambios de kiwi

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -9,23 +9,22 @@ export const Profile = () => {
   const { store, actions } = useContext(Context);
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <div className="profilecard">
-        <img
-          src="https://www.pbs.org/wnet/nature/files/2015/11/636x460design_01-610x441.jpg"
-          alt="John"
-          style={{ width: "100%" }}
-        />
-        <h1>Elena Fornies</h1>
-        <p className="title">CEO &amp; Founder, Example</p>
-        <p>Harvard University</p>
-
-        <p>
-          <button>Contact</button>
-        </p>
+      <div className="covercard" >
+        
+        
+        <div className="profilecard">
+          <img
+            src="https://img.icons8.com/ios/452/kiwi-bird.png"
+            alt="Kiwi Bird Drawing"
+            width={200}
+          />
+          <h1>Elena Fornies</h1>
+          <p className="title">Digital artist </p>
+          <p>30 drawings / 20 folowers</p>
+          <p>
+            <button>Contact</button>
+          </p>
+        </div>
       </div>
     </div>
   );
