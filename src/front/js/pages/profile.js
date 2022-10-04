5f41d6ada885c8b885f191fed81d8cf295a15344
Efornies <eleforbel@gmail.com>
2022-10-04 16:13:46 +0000

    kiwi perfil

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -5,88 +5,28 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee, faKiwiBird } from "@fortawesome/free-solid-svg-icons";
 
-
 export const Profile = () => {
   const { store, actions } = useContext(Context);
   return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <div className="profilecard">
+        <img
+          src="https://www.pbs.org/wnet/nature/files/2015/11/636x460design_01-610x441.jpg"
+          alt="John"
+          style={{ width: "100%" }}
+        />
+        <h1>Elena Fornies</h1>
+        <p className="title">CEO &amp; Founder, Example</p>
+        <p>Harvard University</p>
 
-    <div className="container">
-      <div className="row">
-        <div className="col-xs-1 col-sm-6 col-md-6">
-          <div className="well well-sm">
-            <div className="row">
-              <div className="col-sm-6 col-md-4">
-                <img
-                  src="https://i.pinimg.com/280x280_RS/2e/45/66/2e4566fd829bcf9eb11ccdb5f252b02f.jpg"
-                  alt=""
-                  className="img-rounded img-responsive"
-                />
-              </div>
-              <div className="col-sm-6 col-md-8">
-                <h4>Elena Forniés</h4>
-                <small>
-                  <cite title="San Francisco, USA">
-                    San Francisco, USA{" "}
-                    <i className="glyphicon glyphicon-map-marker"></i>
-                  </cite>
-                </small>
-                <p>
-                  <i className="glyphicon glyphicon-envelope"></i>
-                  email@example.com
-                  <br />
-                  <i className="glyphicon glyphicon-globe"></i>
-                  <a href="http://www.jquery2dotnet.com">
-                    www.jquery2dotnet.com
-                  </a>
-                  <br />
-                  <i className="glyphicon glyphicon-gift"></i>June 02, 1988
-                </p>
-
-                <div className="btn-group">
-                  <button type="button" className="btn btn-primary">
-                    Social
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-primary dropdown-toggle"
-                    data-toggle="dropdown"
-                  >
-                    <span className="caret"></span>
-                    <span className="sr-only">Social</span>
-                  </button>
-                  <ul className="dropdown-menu" role="menu">
-                    <li>
-                      <a href="#">Twitter</a>
-                    </li>
-                    <li>
-                      <a href="https://plus.google.com/+Jquery2dotnet/posts">
-                        Google +
-                      </a>
-                    </li>
-                    <li>
-                      <a href="https://www.facebook.com/jquery2dotnet">
-                        Facebook
-                      </a>
-                    </li>
-                    <li className="divider"></li>
-                    <li>
-                      <a href="#">Github</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div> 
-      <div className="btn-group" role="group" aria-label="Basic example">
-  <button type="button" className="btn btn-secondary">My Art</button>
-  <button type="button" className="btn btn-secondary">My favs</button>
-  <button type="button" className="btn btn-secondary">Following</button>
-</div>
+        <p>
+          <button>Contact</button>
+        </p>
       </div>
     </div>
-    
   );
 };
