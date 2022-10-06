71ff4f9684854032e42f3f03024ae8646cb3c3ac
Efornies <eleforbel@gmail.com>
2022-10-06 12:27:19 +0000

    carousel done

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/profile.css";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee, faKiwiBird } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Carousel from 'react-bootstrap/Carousel';
 import background from "../../img/background_image.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -12,11 +12,11 @@ import { myart } from "../component/myart";
 import { myfavs } from "../component/myfavs";
 import { myfollowers } from "../component/myfollowers";
 
-
-
-
 export const Profile = () => {
   const { store, actions } = useContext(Context);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className="containerprofile">
@@ -31,9 +31,26 @@ export const Profile = () => {
             <h1>New</h1>
             <p className="titleprofile">Digital artist </p>
             <p>30 drawings / 20 folowers</p>
-            <p>
-              <button>Edit</button>
-            </p>
+
+            <button type="button" onClick={handleShow}>
+              Edit
+            </button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit Profile</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                Looking for another look?
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </Col>
 
@@ -48,8 +65,63 @@ export const Profile = () => {
       <Row
         style={{ paddingLeft: "2%", paddingTop: "20px", paddingRight: "2%" }}
       >
-        
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <a className="nav-link active" href="../component/myart">
+              My Art
+            </a>
+          </li>
+          <li className="nav-item">
+            <a class="nav-link" style={{ color: "burlywood" }} href="#">
+              My Favs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" style={{ color: "burlywood" }} href="#">
+              My Kiwis
+            </a>
+          </li>
+        </ul>
       </Row>
+      <Carousel className="ArtPerfil">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="http://prod-upp-image-read.ft.com/e9a0d7ee-a1be-11e8-85da-eeb7a9ce36e4"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Another World</h3>
+          <p>Digital art using PS </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/ab723877268563.5c829061bd6ea.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.pinimg.com/originals/9b/a5/a2/9ba5a220b73eb7cf1eabcbedf3101fe9.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
   );
 };
